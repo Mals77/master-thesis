2ead9d96a0a40ad21e800f51a29bb02a63ebab9b
Mals77 <malou.skiera@gmx.de>
2023-11-09 14:29:32 +0100

    [EDIT] finish enhancement to include sucessfullness

--- enhanceDataset.py
+++ enhanceDataset.py
@@ -25,12 +25,14 @@ def addTreatment(log):
 
 def addSuccessColumns(log):
     # if case includes A_Pending than column successful = 1, else 0
+    log['successful'] = log.groupby('case:concept:name')['concept:name'].transform(lambda x: 1 if 'A_Pending' in x.values else 0)
     # make column treatmentSuccess, here treated and if successfull Yes, else No
-
-#TODO: add column successfull or not successful
+    log['treatmentSuccess'] = log.apply(lambda row: 'Yes' if row['treatment'] == 'treated' and row['successful'] == 1 else 'No' if row['treatment'] == 'treated' and row['successful'] == 0 else '', axis=1)
+    #log['treatmentSuccess'] = log.groupby('case:concept:name').apply(lambda group: "Yes" if ('treated' in group['treatment'].values) and ('successful' == 1) else "No" if ('treated' in group['treatment'].values) else '').reset_index(level=0, drop=True)
+    return log
 
 def enhancement(finishedCases):
     offersLog = add_No_Of_Offers(finishedCases)
     treatedLog = addTreatment(offersLog)
-    # sucessLog = addSuccessColumns(treatedLog)
-    return treatedLog
+    sucessLog = addSuccessColumns(treatedLog)
+    return sucessLog
