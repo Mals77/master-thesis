0a5a13159aa75a5d2a34336c97bb76586a3565e8
mskiera@uni-mannheim.de <ma_mskiera@uc2n269.localdomain>
2024-01-22 11:12:21 +0100

    [ADD] Evaluation to the DoWhy Methods and add their results to the result df

--- .ipynb_checkpoints/evaluationData-checkpoint.py
+++ .ipynb_checkpoints/evaluationData-checkpoint.py
@@ -1,7 +1,8 @@
 import random
+import numpy as np
 import pandas as pd
 
-def bpi2017Feautues(df_final):
+def bpi2017Features(df_final):
     treatment=df_final['treatmentOffer']
     features = df_final[['NumberOfOffers', 'Action', 'org:resource',
        'concept:name', 'EventOrigin', 'lifecycle:transition', 'time:timestamp',
@@ -9,14 +10,14 @@ def bpi2017Feautues(df_final):
        'FirstWithdrawalAmount', 'NumberOfTerms', 'Accepted', 'MonthlyCost', 'Selected',
        'CreditScore', 'OfferedAmount', 'offerNumber','timeApplication', 'weekdayApplication']]
     y=df_final['offerSuccess']
-    return df_final, features, treatment, y
+    return features, treatment, y
 
 def placeboTreatment(data):
     """Replaces the treatment variable with a new variable randomly generated."""
-    inference_features, treatment_col, outcome_col = bpi2017Feautues(data)
+    inference_features, treatment_col, outcome_col = bpi2017Features(data)
     num_rows = data.df.shape[0]
 
-    X = data.df[data.inference_features].values
+    X = data.df[data[inference_features]].values
     treatment_new = np.random.randint(2, size=num_rows)
     y = data.df[data.outcome_col].values
 
@@ -25,12 +26,12 @@ def placeboTreatment(data):
 
 
 def randomCause(data):
-    inference_features, treatment_col, outcome_col = bpi2017Feautues(data)
+    inference_features, treatment_col, outcome_col = bpi2017Features(data)
     """Adds an irrelevant random covariate to the dataframe."""
     num_rows = data.df.shape[0]
     new_data = np.random.randn(num_rows)
 
-    X = data.df[data.inference_features].values
+    X = data.df[data[inference_features]].values
     treatment = data.df[data.treatment_col].values
     y = data.df[data.outcome_col].values
     X_new = np.hstack((X, new_data.reshape((-1, 1))))
@@ -40,31 +41,25 @@ def randomCause(data):
 
 def randomReplace(data):
     """Replaces a random covariate with an irrelevant variable."""
-    inference_features, treatment_col, outcome_col = bpi2017Feautues(data)
+    inference_features, treatment_col, outcome_col = bpi2017Features(data)
     
-    replaced_feature_index = np.random.randint(len(data.inference_features))
-    data.replaced_feature = data.inference_features[replaced_feature_index]
+    replaced_feature_index = np.random.randint(len(inference_features.columns))
+    replaced_feature = inference_features.columns[replaced_feature_index]
+    random_value = np.random.randint(0, 100)
+    inference_features.iloc[:, replaced_feature_index] = random_value
 
-    logger.info(
-        "Replace feature {} with an random irrelevant variable".format(
-            data.replaced_feature
-        )
-    )
-    
-    df_new = data.df.copy()
-    num_rows = data.df.shape[0]
-    df_new[data.replaced_feature] = np.random.randn(num_rows)
+    new_df = pd.concat([inference_features, treatment_col, outcome_col], axis=1)
 
-    X_new = df_new[data.inference_features].values
-    treatment_new = df_new[data.treatment_col].values
-    y_new = df_new[data.outcome_col].values
+    X_new = inference_features.values
+    treatment_new = treatment_col.values
+    y_new = outcome_col.values
 
-    return df_new, X_new, y_new, treatment_new
+    return new_df, X_new, y_new, treatment_new, replaced_feature
 
 
     
 def randomSubsetData(data):
-    inference_features, treatment_col, outcome_col = bpi2017Feautues(data)
+    inference_features, treatment_col, outcome_col = bpi2017Features(data)
     """Takes a random subset of size sample_size of the data."""
     #sample_size is between 0.7 and 0.95 of orginal dataset
     sample_size = random.uniform(0.7, 0.95)
